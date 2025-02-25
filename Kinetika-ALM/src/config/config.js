af11c0b28b19dc9d04f9e8a9912d81ee3ffd191d
Vaibhav <vaibhavagarwal54321@gmail.com>
2025-02-25 11:22:54 +0530

    login

--- Kinetika-ALM/src/config/config.js
+++ Kinetika-ALM/src/config/config.js
@@ -2,16 +2,12 @@ require("dotenv").config();
 
 module.exports = {
   email: {
-    host: "smtpout.secureserver.net",
+    host: "smtp.zoho.com",
     port: 465, // Use 465 for SSL or 587 for TLS
     secure: true, // true for SSL, false for TLS
     auth: {
-      user: "noreply@nexushubs.in", // GoDaddy email
-      pass: "Kedar123!", // GoDaddy email password
+      user: "vaibhav.a@kinetikalabs.com", // GoDaddy email
+      pass: "vaibhava@15", // GoDaddy email password
     },
-  },
-  msg91: {
-    authKey: "437330ArWDUQB2ZuW677033a0P1", // Replace with your MSG91 Auth Key
-    templateId: "67728538d6fc0509bb1cd5f3", // Replace with your MSG91 template ID
   },
 };
